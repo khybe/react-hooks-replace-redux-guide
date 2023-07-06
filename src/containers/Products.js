dd51af2ff01ceb99705ccb4f4f836ed811e7836e
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-06 21:50:46 +0200

    Using the Custom Store instead of Context API

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,15 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import ProductItem from "../components/Products/ProductItem"; // Importing the ProductItem component from the components/Products/ProductItem.js file
-import { ProductsContext } from "../context/products-context";
+import { useStore } from "../hook-store/store";
 import "./Products.css";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
 
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom"; // Importing BrowserRouter component from react-router-dom for client-side routing
 
-import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureProductsStore from "./hook-store/products-store";
+import "./index.css";
+
+configureProductsStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsProvider>
-    {/* Wrapping the App component with the Provider component to provide the Redux store to all components */}
-    <BrowserRouter>
-      {/* Wrapping the App component with the BrowserRouter component to enable client-side routing */}
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
